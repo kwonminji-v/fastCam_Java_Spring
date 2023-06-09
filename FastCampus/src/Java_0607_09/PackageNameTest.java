029373ad2ff27c3444883d98ebda20be80b46a79
minji6313 <minji6313@yahoo.com>
2023-06-09 22:02:12 +0900

    "접근 제어자 까지"

--- FastCampus/src/Java_0607_09/PackageNameTest.java
+++ FastCampus/src/Java_0607_09/PackageNameTest.java
@@ -12,6 +12,7 @@ public class PackageNameTest {
         //java.lang에 보관하고 있는 클래스 이외에는 전부 import를 해주어야 해당 클래스를 사용이 가능합니다. import java.lang.*; 은 눈에 보이진 않지만 default package로써 작성되어 있기 때문에 class full name으로 작성하지 않아도 에러가 나지 않습니다.
 
         //사용자가 정의 해본 클래스들의 이름을 적어보자
-
+        //git pull ㅇ으악
+        //오늘 인강도 못들었는뎅 ㅠㅠ
     }
 }
