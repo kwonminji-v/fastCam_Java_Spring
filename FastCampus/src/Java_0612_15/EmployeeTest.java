b02f22cf24e4e943100523ddad2beedc5fa8c2df
minji6313 <minji6313@yahoo.com>
2023-06-13 18:05:27 +0900

    "Upcasting,Downcasting"

--- FastCampus/src/Java_0612_15/EmployeeTest.java
+++ FastCampus/src/Java_0612_15/EmployeeTest.java
@@ -2,6 +2,9 @@ package Java_0612_15;
 
 public class EmployeeTest {
     public static void main(String[] args) {
+
+
+
         // 일반 사원 한명의 객체를 생성하고 데이터를 저장 후 출력을 하세요.
         RempVO vo = new RempVO("고길동",31,"111-1111-1111","2012-03-23","IT부서",false);
         // vo의 타입은 RempVO가 되며, 상속을 통해 Employee를 포함한 전체 메모리에 접근이 가능해집니다.
@@ -11,6 +14,11 @@ public class EmployeeTest {
 //        System.out.println(vo.name+"\t" + vo.age+"\t"+vo.phone+"\t"+vo.empDate+"\t"+vo.dept+"\t"+vo.marriage);
 
         System.out.println(vo.toString());
+
+
+
+
+
 
         //일반 회원 한사람의 정보가 초기화가 된 상태
         RempVO vo1 = new RempVO("고길동", 51,"222-2222-2222","2022-10-10","홍보부",true);
--- FastCampus/src/model/Dog.java
+++ FastCampus/src/model/Dog.java
@@ -2,6 +2,11 @@ package model;
 
 public class Dog extends Animal{
     //부모클래스에 eat() 라는 메서드가 있고 extends를 통해 부모클래스에 있는 메서드를 사용할 수 있도록 허락해준 상황
+    public Dog(){ //생성자가 { } 내에서 실행이 완료되어야 Dog() 객체가 완벽하게 만들어지는데,
+                  // Animal(부모)부터 만들어진 후 수행문이 실행되어야 하는데 수행문을 먼저 작성되었기 때문에 super()에서 에러가 발생합니다.
+        int a = 10;  // 수행문
+        super();  // new Animal()이 super()에 의해서 호출됨
 
+    }
 
 }
